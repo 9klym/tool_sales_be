be4c8b1d2eba850d2d3db2b13ea59f940c68220f
Alexander Klymenko <alexanderklymenko55@gmail.com>
2021-02-14 19:35:38 +0200

    Add devise gem and user model

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable,
+         :rememberable, :validatable
+  enum role: {
+    director: 'director',
+    accountant: 'accountant',
+    storekeeper: 'storekeeper'
+  }
+  validates :email, presence: true, format: Devise.email_regexp
+  validates :role, presence: true
+  validates :name, presence: true
+  validates :password, length: { minimum: 8 }
+end
